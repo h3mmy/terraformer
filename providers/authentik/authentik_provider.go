849efdd9ff5dddf8d79256c8bac91483e2cfc0fb
Zee Aslam <zeet6613@gmail.com>
2022-05-08 10:53:05 -0400

    Check in

---
+++ providers/authentik/authentik_provider.go
@@ -0,0 +1,96 @@
+// Copyright 2018 The Terraformer Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package authentik
+
+import (
+	"errors"
+	"os"
+	"strconv"
+
+	"github.com/GoogleCloudPlatform/terraformer/terraformutils"
+	"github.com/zclconf/go-cty/cty"
+)
+
+type AuthentikProvider struct { //nolint
+	terraformutils.Provider
+	url       string
+	token        string
+	insecure     bool
+}
+
+func (p *AuthentikProvider) Init(args []string) error {
+	akUrl := os.Getenv("AUTHENTIK_API_URL")
+	if akUrl == "" {
+		return errors.New("set AUTHENTIK_API_URL env var")
+	}
+	p.url = akUrl
+
+	token := os.Getenv("AUTHENTIK_API_TOKEN")
+	if token == "" {
+		return errors.New("set AUTHENTIK_API_TOKEN env var")
+	}
+	p.token = token
+
+	insecure, err := strconv.ParseBool(os.Getenv("AUTHENTIK_INSECURE"))
+	if err != nil {
+		return errors.New("Parsing Error for AUTHENTIK_INSECURE")
+	}
+	p.insecure = insecure
+
+	return nil
+}
+
+func (p *AuthentikProvider) GetName() string {
+	return "authentik"
+}
+
+func (p *AuthentikProvider) GetConfig() cty.Value {
+	return cty.ObjectVal(map[string]cty.Value{
+		"url":        cty.StringVal(p.url),
+		"token":      cty.StringVal(p.token),
+		"insecure":       cty.BoolVal(p.insecure),
+	})
+}
+
+func (p *AuthentikProvider) InitService(serviceName string, verbose bool) error {
+	var isSupported bool
+	if _, isSupported = p.GetSupportedService()[serviceName]; !isSupported {
+		return errors.New(p.GetName() + ": " + serviceName + " not supported service")
+	}
+	p.Service = p.GetSupportedService()[serviceName]
+	p.Service.SetName(serviceName)
+	p.Service.SetVerbose(verbose)
+	p.Service.SetProviderName(p.GetName())
+	p.Service.SetArgs(map[string]interface{}{
+		"url":        p.url,
+		"token":     p.token,
+		"insecure": p.insecure,
+	})
+	return nil
+}
+
+func (p *AuthentikProvider) GetSupportedService() map[string]terraformutils.ServiceGenerator {
+	return map[string]terraformutils.ServiceGenerator{
+		"authentik_user":          &UserGenerator{},
+	}
+}
+
+func (p AuthentikProvider) GetResourceConnections() map[string]map[string][]string {
+	return map[string]map[string][]string{}
+}
+
+func (p AuthentikProvider) GetProviderData(arg ...string) map[string]interface{} {
+	return map[string]interface{}{}
+}
---
+++ providers/authentik/authentik_service.go
@@ -0,0 +1,73 @@
+// Copyright 2018 The Terraformer Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package authentik
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/GoogleCloudPlatform/terraformer/terraformutils"
+	httptransport "github.com/go-openapi/runtime/client"
+	api "goauthentik.io/api/v3"
+)
+
+type AuthentikService struct { //nolint
+	terraformutils.Service
+}
+
+// APIClient Hold the API Client and any relevant configuration
+type APIClient struct {
+	client *api.APIClient
+}
+
+func (s *AuthentikService) generateClient() *APIClient {
+
+	apiURL := s.Args["url"].(string)
+	token := s.Args["token"].(string)
+	insecure := s.Args["insecure"].(bool)
+
+	akURL, err := url.Parse(apiURL)
+	if err != nil {
+		panic(err)
+	}
+
+	config := api.NewConfiguration()
+	config.UserAgent = fmt.Sprintf("authentik-terraformer")
+	config.Host = akURL.Host
+	config.Scheme = akURL.Scheme
+
+	config.AddDefaultHeader("Authorization", fmt.Sprintf("Bearer %s", token))
+	config.HTTPClient = &http.Client{
+		Transport: GetTLSTransport(insecure),
+	}
+	apiClient := api.NewAPIClient(config)
+
+
+	return &APIClient{
+		client: apiClient,
+	}
+}
+
+// GetTLSTransport Get a TLS transport instance, that skips verification if configured via environment variables.
+func GetTLSTransport(insecure bool) http.RoundTripper {
+	tlsTransport, err := httptransport.TLSTransport(httptransport.TLSClientOptions{
+		InsecureSkipVerify: insecure,
+	})
+	if err != nil {
+		panic(err)
+	}
+	return tlsTransport
+}
